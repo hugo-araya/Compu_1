238fc87167ba1e65633f51eeb04968f4a8f318f9
hugo-araya <hugo.araya.carrasco@gmail.com>
2021-09-30 16:29:22 -0300

    30

--- ganancia.py
+++ ganancia.py
@@ -3,6 +3,3 @@ cap_invertir = float(input('Ingrese capital a invertir: '))
 interes = float(input('% interes: '))
 ganancia = cap_invertir * interes/100
 print('Su ganancia mensual es:',ganancia,"al",interes,"%")
-
-
-
--- primer.py
+++ primer.py
@@ -1,4 +1,10 @@
-a = 5
+a = 20
 b = 2
 c = a % b
 print('El resultado es: ', c)
+
+if a < 10:
+    print ('Es menor que 10')
+else:
+    print ('No es menor que 10')
+print("Continua...")

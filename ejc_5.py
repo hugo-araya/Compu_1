5fdaa653cc5f401337dfcc72f2c73d2fb4ba705a
hugo-araya <hugo.araya.carrasco@gmail.com>
2021-11-11 18:07:36 -0300

    12

--- ejc_5.py
+++ ejc_5.py
@@ -1,23 +1,29 @@
 def lectura_datos(archivo):
+    original = []
     ent = open(archivo)
-    linea = ent.readline()
-    linea = linea.rstrip('\n')
-    lista = linea.split(' ')
-    nombre = lista[0]
-    apellido = lista[1]
-    ingreso = lista[2]
-    return nombre, apellido, ingreso
+    for linea in ent:
+        linea = linea.rstrip('\n')
+        lista = linea.split(' ')
+        original.append(lista)
+    return original
 
-def proceso(nombre, apellido, ingreso):
-    salida = ingreso[2:] + nombre[0] + apellido
-    return salida
+def proceso(original):
+    lista_salida = []
+    for linea in original:
+        nombre = linea[0]
+        apellido = linea[1]
+        ingreso = linea[2]
+        salida = ingreso[2:] + nombre[0] + apellido
+        lista_salida.append(salida)
+    return lista_salida
 
-def salida_datos(salida):
+def salida_datos(lista_salida):
     print('-----------------------------------')
-    print('| Lo solicitado es: ', salida)
+    for elem in lista_salida:
+        print('| Lo solicitado es: ', elem)
     print('-----------------------------------')
 
 if __name__ == '__main__':
-    nombre, apellido, ingreso = lectura_datos('ejc_datos.txt')
-    salida = proceso(nombre, apellido, ingreso)
-    salida_datos(salida)
+    original = lectura_datos('ejc_datos.txt')
+    lista_salida = proceso(original)
+    salida_datos(lista_salida)
